d2695c229632f9cfffd653af1a6ad59c90b7bee7
Angrbody <13522017717@163.com>
2021-11-23 17:06:50 +0800

    final2

--- solutions/str_to_int/test.cpp
+++ solutions/str_to_int/test.cpp
@@ -1,39 +1,27 @@
 #include<iostream>
 #include<string>
 using namespace std;
-int main() {
+int get_int(string ss){
+    int index = 0;
+    int res = 0;
+    int bdy = 2^31-1;
+    while(ss[index] >= '0' && ss[index] <= '9'){
+        res = res*10 + (ss[index]-'0');
+        index++;
+    }
+    if(res > bdy) res = bdy;
 
-        string str = "-+1";
-        int res = 0, index = 0, sign = 1;   //符号位默认为1（正）
-        int bndry = 1000010;
-        int l = str.size();
-        if(l == 0)  return 0;
-        while(str[index] == ' '){
-            index++;
-        }
-        if(index == l)  return 0;
-        if(str[index] == '-'){
-            index++;
-            sign = -1;
-        }
-        if(str[index] == '+'){
-            index++;
-        }
-        if(abs(sign)!=1) return 0;
-        for(int i = index; i<l; i++){
-            if(str[i]<'0' || str[i]>'9')    break;
-            if(res>bndry ||(res==bndry && str[i]>'7')){
-                return sign == 1 ? 1000000:100000;
-            }
-            res = res * 10 + (str[i] - '0');
-            cout<<"computing... , res = "<<res<<endl;
-        }
-        cout<<"index = "<<index<<endl;
-        cout<<"length = "<<l<<endl;
-        cout<<"res = "<<res<<endl;
+    return res;
+}
 
-    
-    system("pause");
-    return 0;
-    
-};
+int main(){
+    string s = "42";
+    int res = 0;
+    for(int i = 0; i<2; i++){
+        res = res * 10 + (s[i] - '0');
+        cout<<"result = "<<res<<endl;
+    }
+
+system("pause");
+return 0;
+}
